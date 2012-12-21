5cff4e30c5f4cdeec2b7cab06a252f8b92ebedbc
Domínio L <lucas@dominiol.com.br>
2012-12-21 13:40:47 -0200

    Ajustes na construição.

--- construir.sh
+++ construir.sh
@@ -2,46 +2,66 @@
 
 fontes=fontes
 fontesJava=${fontes}/java
+
 testes=testes
 testesJava=${testes}/java
+
 binarios=binarios
 binariosJava=${binarios}/class
+
 bibliotecas=bibliotecas
 bibliotecasJava=${bibliotecas}/jar
 bibliotecaJUnit=${bibliotecasJava}/jUnit.jar
+
 construcao=construcao
 arquivosFontesJava=${construcao}/arquivosFontesJava.txt
 arquivosTestesJava=${construcao}/arquivosTestesJava.txt
 relatorios=${construcao}/relatorios
 relatorioTestesJava=${relatorios}/testesJava.txt
 relatorioTestesJavaScript=${relatorios}/testesJavaScript.txt
+
 recursos=recursos
 
-echo ":limpar"
-rm -rf ${binarios}
-rm -rf ${construcao}
+limpar() {
+	echo ":limpar";
+	rm -rf ${binarios};
+	rm -rf ${construcao};
+}
 
-echo ":criarDiretorios"
-mkdir -p ${fontes}
-mkdir -p ${fontesJava}
-mkdir -p ${testes}
-mkdir -p ${testesJava}
-mkdir -p ${binarios}
-mkdir -p ${binariosJava}
-mkdir -p ${bibliotecas}
-mkdir -p ${bibliotecasJava}
-mkdir -p ${construcao}
-mkdir -p ${relatorios}
+criarDiretorios() {
+	echo ":criarDiretorios"
+	mkdir -p ${fontes};
+	mkdir -p ${fontesJava};
+	mkdir -p ${testes};
+	mkdir -p ${testesJava};
+	mkdir -p ${binarios};
+	mkdir -p ${binariosJava};
+	mkdir -p ${bibliotecas};
+	mkdir -p ${bibliotecasJava};
+	mkdir -p ${construcao};
+	mkdir -p ${relatorios};
+}
 
-echo ":compilarFontesJava"
-find ${fontesJava} -name *.java > ${arquivosFontesJava}
-javac -g -deprecation -Werror -classpath ${bibliotecaJUnit}:${binariosJava} -sourcepath ${fontesJava} -d ${binariosJava} @${arquivosFontesJava}
+compilarFontesJava() {
+	echo ":compilarFontesJava";
+	find ${fontesJava} -name *.java > ${arquivosFontesJava};
+	javac -g -deprecation -Werror -classpath ${bibliotecaJUnit}:${binariosJava} -sourcepath ${fontesJava} -d ${binariosJava} @${arquivosFontesJava};
+}
 
-echo ":compilarTestesJava"
-find ${testesJava} -name *.java > ${arquivosTestesJava}
-javac -g -deprecation -Werror -classpath ${bibliotecaJUnit}:${binariosJava} -sourcepath ${fontesJava}:${testesJava} -d ${binariosJava} @${arquivosTestesJava}
+compilarTestesJava() {
+	echo ":compilarTestesJava";
+	find ${testesJava} -name *.java > ${arquivosTestesJava};
+	javac -g -deprecation -Werror -classpath ${bibliotecaJUnit}:${binariosJava} -sourcepath ${fontesJava}:${testesJava} -d ${binariosJava} @${arquivosTestesJava};
+}
 
-echo ":testar"
-classesTestesJava=$(sed -e s:${testesJava}::1 -e s:/:.:g -e s:.java:: -e s:[a-Z.]*figuracao[a-Z.]*:: -e s:^.:: < ${arquivosTestesJava})
-java -classpath ${bibliotecaJUnit}:${binariosJava} org.junit.runner.JUnitCore ${classesTestesJava} > ${relatorioTestesJava}
+testar() {
+	echo ":testar";
+	classesTestesJava=$(sed -e s:${testesJava}::1 -e s:/:.:g -e s:.java:: -e s:[a-Z.]*figuracao[a-Z.]*:: -e s:^.:: < ${arquivosTestesJava});
+	java -classpath ${bibliotecaJUnit}:${binariosJava} org.junit.runner.JUnitCore ${classesTestesJava} > ${relatorioTestesJava};
+}
 
+limpar;
+criarDiretorios;
+compilarFontesJava;
+compilarTestesJava;
+testar;
