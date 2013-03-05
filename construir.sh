e39c7757241d79b92ebd3ae3e337b4c6517cdbfc
Domínio L <lucas@dominiol.com.br>
2013-03-05 07:26:02 -0300

    Alteração no arquivo de construção.

--- construir.sh
+++ construir.sh
@@ -1,26 +1,19 @@
-#!/bin/bash
-
+projeto=estruturados
 fontes=fontes
-fontesJava=${fontes}/java
-
 testes=testes
-testesJava=${testes}/java
-
+bibliotecas=bibliotecas
+recursos=recursos
 binarios=binarios
+construcao=construcao
+
+fontesJava=${fontes}/java
+testesJava=${testes}/java
+bibliotecasJava=${bibliotecas}/jar
 binariosJava=${binarios}/class
 
-bibliotecas=bibliotecas
-bibliotecasJava=${bibliotecas}/jar
-bibliotecaJUnit=${bibliotecasJava}/jUnit.jar
-
-construcao=construcao
-arquivosFontesJava=${construcao}/arquivosFontesJava.txt
-arquivosTestesJava=${construcao}/arquivosTestesJava.txt
-relatorios=${construcao}/relatorios
-relatorioTestesJava=${relatorios}/testesJava.txt
-relatorioTestesJavaScript=${relatorios}/testesJavaScript.txt
-
-recursos=recursos
+arquivosFontesJava=$(find ${fontesJava} -name *.java)
+arquivosTestesJava=$(find ${testesJava} -name *.java)
+classesTestesJava=$(echo ${arquivosTestesJava} | sed -e s:${testesJava}::g -e s:^/::g -e "s:\s/: :g" -e s:/:.:g -e s:[.]java::g -e s:[a-Z.]*figuracao[a-Z.]*::g)
 
 limpar() {
 	echo ":limpar";
@@ -28,57 +21,53 @@ limpar() {
 	rm -rf ${construcao};
 }
 
-criarDiretorios() {
-	echo ":criarDiretorios"
-	mkdir -p ${fontes};
+criarEstrutura() {
+	echo ":criarEstrutura";
 	mkdir -p ${fontesJava};
-	mkdir -p ${testes};
 	mkdir -p ${testesJava};
-	mkdir -p ${binarios};
-	mkdir -p ${binariosJava};
-	mkdir -p ${bibliotecas};
 	mkdir -p ${bibliotecasJava};
+	mkdir -p ${binariosJava};
 	mkdir -p ${construcao};
-	mkdir -p ${relatorios};
 }
 
-compilarFontesJava() {
-	echo ":compilarFontesJava";
-	find ${fontesJava} -name *.java > ${arquivosFontesJava};
-	javac -g -deprecation -Werror -classpath ${bibliotecaJUnit}:${binariosJava} -sourcepath ${fontesJava} -d ${binariosJava} @${arquivosFontesJava};
+adicionarBibliotecas() {
+	echo ":adicionarBibliotecas";
 }
 
-compilarTestesJava() {
-	echo ":compilarTestesJava";
-	find ${testesJava} -name *.java > ${arquivosTestesJava};
-	javac -g -deprecation -Werror -classpath ${bibliotecaJUnit}:${binariosJava} -sourcepath ${fontesJava}:${testesJava} -d ${binariosJava} @${arquivosTestesJava};
+construir() {
+	limpar;
+	criarEstrutura;
+	adicionarBibliotecas;
+}
+
+compilar() {
+	construir;
+	echo ":compilar";
+	javac -classpath ${bibliotecasJava}/*:${binariosJava} -sourcepath ${fontesJava} -d ${binariosJava} -Werror -deprecation -g ${arquivosFontesJava}
+ 	javac -classpath ${bibliotecasJava}/*:${binariosJava} -sourcepath ${testesJava} -d ${binariosJava} -Werror -deprecation -g ${arquivosTestesJava}
 }
 
 testar() {
 	compilar;
 	echo ":testar";
-	classesTestesJava=$(sed -e s:${testesJava}::1 -e s:/:.:g -e s:.java:: -e s:[a-Z.]*figuracao[a-Z.]*:: -e s:^.:: < ${arquivosTestesJava});
-	#java -classpath ${bibliotecaJUnit}:${binariosJava} org.junit.runner.JUnitCore ${classesTestesJava} > ${relatorioTestesJava};
-	java -classpath ${bibliotecaJUnit}:${binariosJava} org.junit.runner.JUnitCore ${classesTestesJava};
+	java -classpath ${bibliotecasJava}/*:${binariosJava} org.junit.runner.JUnitCore ${classesTestesJava};
 }
 
 depurar() {
 	compilar;
 	echo ":depurar";
-	classesTestesJava=$(sed -e s:${testesJava}::1 -e s:/:.:g -e s:.java:: -e s:[a-Z.]*figuracao[a-Z.]*:: -e s:^.:: < ${arquivosTestesJava});
-	jdb -classpath ${bibliotecaJUnit}:${binariosJava} org.junit.runner.JUnitCore ${classesTestesJava};
+	jdb -classpath ${bibliotecasJava}/*:${binariosJava} org.junit.runner.JUnitCore ${classesTestesJava};
 }
 
-compilar() {
-	limpar;
-	criarDiretorios;
-	compilarFontesJava;
-	compilarTestesJava;
+gerarVersao() {
+	compilar;
+	echo ":gerarVersao";
+	jar -cf ${construcao}/${projeto}.jar -C ${binariosJava} .
 }
 
 if [ -n "$1" ]
 then
 	$1;
 else
-	compilar;
+	construir;
 fi
