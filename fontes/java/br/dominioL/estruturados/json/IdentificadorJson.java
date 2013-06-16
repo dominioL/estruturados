a6dff99940e605d7fd36be2094f2833bf9c15b0a
Domínio L <lucas@dominiol.com.br>
2013-06-16 17:36:40 -0300

    Remoção de tabulações desnecessárias.

--- fontes/java/br/dominioL/estruturados/json/IdentificadorJson.java
+++ fontes/java/br/dominioL/estruturados/json/IdentificadorJson.java
@@ -4,11 +4,11 @@ import br.dominioL.estruturados.elemento.Codificavel;
 
 public final class IdentificadorJson implements Codificavel<IdentificadorJson> {
 	private String identificador;
-	
+
 	protected IdentificadorJson(String identificador) {
 		this.identificador = identificador;
 	}
-	
+
 	public String fornecerComoJson() {
 		StringBuilder textoJson = new StringBuilder();
 		textoJson.append(ValorJson.DELIMITADOR_TEXTUAL);
@@ -17,12 +17,12 @@ public final class IdentificadorJson implements Codificavel<IdentificadorJson> {
 		textoJson.append(ValorJson.SEPARADOR_DE_IDENTIFICADOR);
 		return textoJson.toString();
 	}
-	
+
 	@Override
 	public Boolean igual(IdentificadorJson outro) {
 		return this.identificador.equals(outro.identificador);
 	}
-	
+
 	@Override
 	public Integer fornecerCodigo() {
 		return identificador.hashCode();
